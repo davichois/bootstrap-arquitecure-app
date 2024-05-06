08d034fa6a45104b9da6b62882540341151a48ae
david prada linarez <davidprali27@gmail.com>
2024-05-05 21:59:39 -0500

    feat: integrate prism, tailwind

--- main.js
+++ main.js
@@ -1,5 +1,22 @@
-let h1 = document.querySelector("h1");
+// Crear una instancia de JSZip
+var zip = new JSZip();
 
-h1.addEventListener("click", () => {
-  alert("Ahora me distes un click de interaccion");
+// Crear la primera carpeta
+var folder1 = zip.folder("Carpeta1");
+
+// Agregar un archivo dentro de la primera carpeta con el contenido "Hola Mundo 1"
+folder1.file("archivo1.txt", "Hola Mundo 1");
+
+// Crear la segunda carpeta
+var folder2 = zip.folder("Carpeta2");
+
+// Agregar dos archivos dentro de la segunda carpeta
+folder2.file("archivo2.txt", "Contenido del archivo 2");
+folder2.file("archivo3.txt", "Contenido del archivo 3");
+
+// Generar el archivo ZIP
+/*zip.generateAsync({ type: "blob" }).then(function (content) {
+  // Guardar el archivo ZIP usando FileSaver.js
+  saveAs(content, "archivos.zip");
 });
+*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./**/*.{html,js}"],
-    theme: {
-      extend: {},
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-primary": "#1B1F23",
+        "bg-primary-live": "#262A2D",
+        "line": "#4A5053",
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
 
-  // npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
+// npx tailwindcss -i ./src/input.css -o ./src/output.css --watch
